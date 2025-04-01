986c1ee3d3b3f600873d7c6da09aa859d35eaa74
Yikun Lv <38689909+Rogerlv51@users.noreply.github.com>
2025-04-01 10:21:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include "yolov8_seg_onnx.h"
 
-using namespace std;
 using namespace cv;
-using namespace dnn;
 
-Yolov8SegOnnx seg_test;
+int main() {
+	Yolov8SegOnnx cengshu_model;
+	cengshu_model.ReadModel("line1_cengshu.onnx");
+	cengshu_model._classThreshold = 0.78;
+	cengshu_model._nmsThreshold = 0.85;
+    cengshu_model._maskThreshold = 0.7;
 
-int main(){
-    if(!=seg_test.ReadModel("best.onnx", false)){
-        cout << "未成功加载模型" << endl;
-        return 0;
+    std::string pattern2 = "C:/Users/Administrator/Desktop/行李实例分割/ultralytics-main/datasets/line1/images/*.jpg";
+    std::vector<cv::String> filenames2;
+    int num = 0;
+    cv::glob(pattern2, filenames2, false);
+    for (const auto& filename : filenames2) {
+        cv::Mat img = cv::imread(filename);
+        std::vector<OutputSeg> outPut;
+        cengshu_model.OnnxDetect(img, outPut);
+        Mat* outImg = new Mat(img.size(), CV_8UC3);
+        cengshu_model.DrawPred(img, outImg, outPut, cengshu_model._className);
+        imshow("out", *outImg);
+        cv::waitKey(0);
+        int num = 0;
+        for (int i = 0; i < outPut.size(); i++) {
+            std::cout << outPut[i].confidence << std::endl;
+            std::cout << "掩码个数：" << outPut[i].boxMask.size() << std::endl;
+            num++;
+        }
+        int test = cengshu_model.compMaskAndInstances(outPut, 0.5);
+        std::cout << "合并后实例数：" << test << std::endl;
     }
-    Mat img = imread("test.jpg");
-    vector<OutputSeg> output;
-    seg_test.OnnxDetect(img, output);
-    Mat* pred_out = new Mat(img.rows, img.cols, CV_8UC3);
-    seg_test.DrawPred(img, pred_out, output, seg_test._className);
-    imshow("pred_img", pred_out);
-    waitKey(0);
-    return 0;
+	
+	return 0;
 }
-
